0d38b092d964f38dda86e8ff8ac52c42455797c6
felipewilliam093@gmail.com <>
2025-09-15 19:47:35 -0300

    Inicializando repositório Escola

---
+++ src/main/java/com/mycompany/escola/Escola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.escola;
+
+/**
+ *
+ * @author william.flima4
+ */
+public class Escola {
+
+    public static void main(String[] args) {
+        
+    }
+}
