5f8411e00d9e644ea23baf366ec22c681fdfb5d9
felipewilliam093-design <felipewilliam093@gmail.com>
2025-09-17 20:51:39 -0300

    Criação do objeto Aluno
    implementação do botão cadastrar.

---
+++ src/main/java/Objetos/Aluno.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+public class Aluno {
+    private String nome;
+    private String email;
+    private String telefone;
+    private String nascimento;
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    public String getEmail() {
+        return email;
+    }
+
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+    public String getTelefone() {
+        return telefone;
+    }
+
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+    
+    
+}
